4741ba0928634466f901ef385f5cc9f1cd0d5bf5
FATIH MEHMET VARLI <fath.varl@gmail.com>
2023-07-12 15:12:58 +0300

    add example and publish

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import {MicroframeTextField} from '../src'
+import { MicroframeTextField } from '../src'
 
 describe('Common render', () => {
-    it('renders without crashing', () => {
-        render(<MicroframeTextField/>)
-    })
+  it('renders without crashing', () => {
+    render(<MicroframeTextField />)
+  })
 })
